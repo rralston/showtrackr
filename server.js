7a1dffa7526913cb888efe691e5f374ac7c0371f
Rupert Ralston <rralston@gmail.com>
2014-06-11 18:34:03 +0100

    Filtering / Categories Angular up and running

--- server.js
+++ server.js
@@ -1,3 +1,57 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+var showSchema = new mongoose.Schema({
+  _id: Number,
+  name: String,
+  airsDayOfWeek: String,
+  airsTime: String,
+  firstAired: Date,
+  genre: [String],
+  network: String,
+  overview: String,
+  rating: Number,
+  ratingCount: Number,
+  status: String,
+  poster: String,
+  subscribers: [{
+    type: mongoose.Schema.Types.ObjectId, ref: 'User'
+  }],
+  episodes: [{
+      season: Number,
+      episodeNumber: Number,
+      episodeName: String,
+      firstAired: Date,
+      overview: String
+  }]
+});
+var userSchema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  password: String
+});
+
+userSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+var User = mongoose.model('User', userSchema);
+var Show = mongoose.model('Show', showSchema);
+mongoose.connect('localhost');
+
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
@@ -15,4 +69,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+app.get('/api/shows', function(req, res, next) {
+  var query = Show.find();
+  if (req.query.genre) {
+    query.where({ genre: req.query.genre });
+  } else if (req.query.alphabet) {
+    query.where({ name: new RegExp('^' + '[' + req.query.alphabet + ']', 'i') });
+  } else {
+    query.limit(12);
+  }
+  query.exec(function(err, shows) {
+    if (err) return next(err);
+    res.send(shows);
+  });
+});
+app.get('/api/shows/:id', function(req, res, next) {
+  Show.findById(req.params.id, function(err, show) {
+    if (err) return next(err);
+    res.send(show);
+  });
+});
+app.get('*', function(req, res) {
+  res.redirect('/#' + req.originalUrl);
+});
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.send(500, { message: err.message });
+});
+.otherwise({
+ redirectTo: '/'
 });
