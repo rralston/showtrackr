be3c428c9466f74bf9f6acfb318be2c33b3c8a7f
Rupert Ralston <rralston@gmail.com>
2014-06-16 12:33:44 +0100

    Installed Passport.js

--- server.js
+++ server.js
@@ -52,13 +52,13 @@ var User = mongoose.model('User', userSchema);
 var Show = mongoose.model('Show', showSchema);
 mongoose.connect('localhost');
 
+var app = express();
+
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
@@ -71,6 +71,10 @@ app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
 
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) next();
+  else res.send(401);
+}
 
 app.get('/api/shows', function(req, res, next) {
   var query = Show.find();
@@ -175,6 +179,30 @@ var async = require('async');
 var request = require('request');
 var xml2js = require('xml2js');
 var _ = require('lodash');
+
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, password, done) {
+  User.findOne({ email: email }, function(err, user) {
+    if (err) return done(err);
+    if (!user) return done(null, false);
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) return done(err);
+      if (isMatch) return done(null, user);
+      return done(null, false);
+    });
+  });
+}));
 app.get('*', function(req, res) {
   res.redirect('/#' + req.originalUrl);
 });
