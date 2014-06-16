75681e1e5a35b730a96cda0f6c7fed6af925c95b
Rupert Ralston <rralston@gmail.com>
2014-06-16 14:56:21 +0100

    Sign-Up and Login Authentication Working

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-var mongoose = require('mongoose');
+var path = require('path');
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+
 var bcrypt = require('bcryptjs');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var async = require('async');
+var request = require('request');
+var xml2js = require('xml2js');
+
+var _ = require('lodash');
+
 var showSchema = new mongoose.Schema({
   _id: Number,
   name: String,
@@ -24,6 +40,7 @@ var showSchema = new mongoose.Schema({
       overview: String
   }]
 });
+
 var userSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   password: String
@@ -48,33 +65,77 @@ userSchema.methods.comparePassword = function(candidatePassword, cb) {
     cb(null, isMatch);
   });
 };
+
 var User = mongoose.model('User', userSchema);
 var Show = mongoose.model('Show', showSchema);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, password, done) {
+  User.findOne({ email: email }, function(err, user) {
+    if (err) return done(err);
+    if (!user) return done(null, false);
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) return done(err);
+      if (isMatch) return done(null, user);
+      return done(null, false);
+    });
+  });
+}));
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) next();
+  else res.send(401);
+}
+
 mongoose.connect('localhost');
 
 var app = express();
-
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
+app.use(function(req, res, next) {
+  if (req.user) {
+    res.cookie('user', JSON.stringify(req.user));
+  }
+  next();
 });
 
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) next();
-  else res.send(401);
-}
+app.post('/api/login', passport.authenticate('local'), function(req, res) {
+  res.cookie('user', JSON.stringify(req.user));
+  res.send(req.user);
+});
+
+app.get('/api/logout', function(req, res) {
+  req.logout();
+  res.send(200);
+});
+
+app.post('/api/signup', function(req, res, next) {
+  var user = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+  user.save(function(err) {
+    if (err) return next(err);
+    res.send(200);
+  });
+});
 
 app.get('/api/shows', function(req, res, next) {
   var query = Show.find();
@@ -90,12 +151,37 @@ app.get('/api/shows', function(req, res, next) {
     res.send(shows);
   });
 });
+
 app.get('/api/shows/:id', function(req, res, next) {
   Show.findById(req.params.id, function(err, show) {
     if (err) return next(err);
     res.send(show);
   });
 });
+
+app.post('/api/subscribe', ensureAuthenticated, function(req, res, next) {
+  Show.findById(req.body.showId, function(err, show) {
+    if (err) return next(err);
+    show.subscribers.push(req.user.id);
+    show.save(function(err) {
+      if (err) return next(err);
+      res.send(200);
+    });
+  });
+});
+
+app.post('/api/unsubscribe', ensureAuthenticated, function(req, res, next) {
+  Show.findById(req.body.showId, function(err, show) {
+    if (err) return next(err);
+    var index = show.subscribers.indexOf(req.user.id);
+    show.subscribers.splice(index, 1);
+    show.save(function(err) {
+      if (err) return next(err);
+      res.send(200);
+    });
+  });
+});
+
 app.post('/api/shows', function(req, res, next) {
   var apiKey = '9EF1D1E7D28FDA0B';
   var parser = xml2js.Parser({
@@ -106,7 +192,7 @@ app.post('/api/shows', function(req, res, next) {
     .toLowerCase()
     .replace(/ /g, '_')
     .replace(/[^\w-]+/g, '');
-  
+
   async.waterfall([
     function(callback) {
       request.get('http://thetvdb.com/api/GetSeries.php?seriesname=' + seriesName, function(error, response, body) {
@@ -171,42 +257,22 @@ app.post('/api/shows', function(req, res, next) {
         }
         return next(err);
       }
+      var alertDate = Date.create('Next ' + show.airsDayOfWeek + ' at ' + show.airsTime).rewind({ hour: 2});
+      agenda.schedule(alertDate, 'send email alert', show.name).repeatEvery('1 week');
       res.send(200);
     });
   });
 });
-var async = require('async');
-var request = require('request');
-var xml2js = require('xml2js');
-var _ = require('lodash');
 
-var session = require('express-session');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
-passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, password, done) {
-  User.findOne({ email: email }, function(err, user) {
-    if (err) return done(err);
-    if (!user) return done(null, false);
-    user.comparePassword(password, function(err, isMatch) {
-      if (err) return done(err);
-      if (isMatch) return done(null, user);
-      return done(null, false);
-    });
-  });
-}));
 app.get('*', function(req, res) {
   res.redirect('/#' + req.originalUrl);
 });
+
 app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.send(500, { message: err.message });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
 });
